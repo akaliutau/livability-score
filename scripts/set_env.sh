0fb1aa3378a86dd7db53c87883771d6b375ca15d
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2024-12-13 23:29:34 +0000

    updated readme

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -1,4 +1,8 @@
+# Check if the required environment variable is set
+if [[ -z "${OPENWEATHER_API_KEY:-}" ]]; then
+  echo "Error: Environment variable 'OPENWEATHER_API_KEY' is not set. Signup at https://openweathermap.org/api/ and get the api key" >&2
+  return 1  # Return with an error status
+fi
 export PROJECT_ID=dev-iot-application
-export OPENWEATHER_API_KEY=
 export HOME_ID=1
 export IS_LOCAL_ENV=true
